5372919172075404b8235a32b84a687b8a629b36
Walisson Gomes <walissonwaal@gmail.com>
2022-09-26 17:29:15 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Libs
+
+- yarn add @react-navigation/native
+- yarn add react-native-screens react-native-safe-area-context
+- yarn add react-native-vector-icons
+- yarn add @react-navigation/stack
+- yarn add react-native-gesture-handler
+- yarn add @react-native-masked-view/masked-view
+- yarn add react-native-elements
---
+++ src/data/users.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      id: 1,
+      name: 'Walisson Gomes',
+      email: 'wall@email.com',
+      avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+    },
+    {
+      id: 2,
+      name: 'João Junior',
+      email: 'joao@email.com',
+      avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png',
+    }
+  ];
+  
